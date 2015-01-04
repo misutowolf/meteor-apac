06a009e98960533c376c5b4f41053290e2942753
alexsmr <smirnoff@trillint.com>
2015-01-03 16:25:58 -0800

    fixing package bumping versions up

--- package.js
+++ package.js
@@ -1,22 +1,17 @@
 Package.describe({
   summary: "Wrapper for the Amazon Product Advertising API NPM module, apac",
   version: "0.0.1",
-  git: "https://github.com/misutowolf/meteor-apac.git",
-  name: "misutowolfe:apac"
+  git: "https://github.com/alexsmr/meteor-apac.git",
+  name: "alexsmr:apac"
 });
 
 Npm.depends({
-  "apac": "0.0.14"
+  "apac": "1.0.0"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0.1');
+  api.versionsFrom('METEOR@1.0.2.1');
   api.addFiles('server/lib/meteor-apac.js','server');
   api.export('apac');
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('meteor-apac');
-  api.addFiles('meteor-apac-tests.js');
-});
